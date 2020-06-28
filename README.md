cf0180ef812359ce6625f6641616df720924c524
Marco Gorelli <m.e.gorelli@gmail.com>
2020-06-28 14:55:17 +0100

    :tada:
    no token

---
+++ README.md
@@ -0,0 +1,11 @@
+To get started:
+
+Within a Python3 virtual environment, run
+```bash
+pip install requests
+```
+
+Then:
+```
+python get_stats.py
+```
---
+++ get_stats.py
@@ -0,0 +1,37 @@
+import requests
+import os
+
+data = requests.get('https://api.github.com/search/issues?q=is:pr+repo:pandas-dev/pandas+author:marcogorelli+is:merged').json()
+
+print(f"Merged PRs: {data['total_count']}")
+
+url = 'https://api.github.com/graphql'
+query = (
+"""
+{
+  user(login: "marcogorelli") {
+    contributionsCollection {
+      pullRequestReviewContributionsByRepository {
+        contributions {
+          totalCount
+        }
+        repository {
+          name
+        }
+      }
+    }
+  }
+}
+"""
+)
+json = { 'query' : query}
+api_token = os.environ['GITSTATS-TOKEN']
+headers = {'Authorization': 'token %s' % api_token}
+
+r = requests.post(url=url, json=json, headers=headers)
+
+json_data = r.json()['data']['user']['contributionsCollection']['pullRequestReviewContributionsByRepository']
+
+reviews = [i['contributions']['totalCount'] for i in json_data if i['repository']['name'] == 'pandas'][0]
+
+print(f'Number of reviews: {reviews}')
